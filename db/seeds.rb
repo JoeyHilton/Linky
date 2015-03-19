561e28d36e4fbbfb4d6dab4521412af90f51fb15
Paul Cox <paulcox24@gmail.com>
2015-03-19 13:37:07 -0600

    Seed for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(email: "test@test.com", name: "Boss", password: "password", password_confirmation: "password")
 
-30.times do |i|
+
+5.times do 
+  User.create(email: Faker::Internet.email,
+              name: Faker::Name.name,
+              password: "password",
+              password_confirmation: "password")
+end
+
+30.times do
   Link.create(title: Faker::Lorem.sentence,
               url: Faker::Internet.url,
               votes: rand(-100..100))
 end
+
+
