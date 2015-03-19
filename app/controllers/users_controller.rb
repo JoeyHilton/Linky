ae7bee50ae9a3b2b30c05ba2a521f95730ecf693
Paul Cox <paulcox24@gmail.com>
2015-03-19 11:44:47 -0600

    Refactor users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,16 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @users = User.all
+  end
+
   def profile
-    @users = current_user
+    @user = current_user
   end
 
   def show
     @user = User.find(params[:id])
   end
 
-  def index
-    @users = User.all
-  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,16 +3,4 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-
-  def show
-    @user = User.find(params[:id])
-  end
-
-  def index
-    @users = User.all
-  end
-
-  def profile
-    @user = current_user
-  end
 end
